329cf4a9728d0c191a01182fd018dea19a1a014a
EPINECNG <info@epinecng.com>
2023-04-17 14:25:21 +0100

    feat: sign in; sign up pages

---
+++ README.md
@@ -0,0 +1,1 @@
+ProjectPlus is a satellite monitoring products that helps to protect critical infrastructure.
---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const passwordInput = document.querySelector("#inputPassword")
+const eye = document.querySelector("#eye")
+
+eye.addEventListener("click", function(){
+    this.classList.toggle("fa-eye-slash")
+    const type = passwordInput.getAttribute("type") === "password" ? "text" : "password"
+    passwordInput.setAttribute("type", type)
+  })
